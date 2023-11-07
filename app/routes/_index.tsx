62faccadd776de5ec9bd489cba7121149584a28c
Andrew Marek <andwmarek@gmail.com>
2023-11-06 17:48:16 -0800

    update server and client to kinda work

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,9 +1,14 @@
-import { json, type ActionFunctionArgs, type MetaFunction, redirect } from "@remix-run/node";
+import {
+  json,
+  type ActionFunctionArgs,
+  type MetaFunction,
+  redirect,
+} from "@remix-run/node";
 import { Form, useActionData } from "@remix-run/react";
 import { v4 } from "uuid";
 import fs from "fs";
-import { fileURLToPath } from 'url';
-import { dirname } from 'path';
+import { fileURLToPath } from "url";
+import { dirname } from "path";
 
 export const meta: MetaFunction = () => {
   return [
@@ -23,30 +28,33 @@ async function createChat(props: IChat) {
 
   let chatUuid = v4();
 
-  fs.readFile(jsonDirectory + "/chatRooms.json", "utf8", (err, data) => { 
+  fs.readFile(jsonDirectory + "/chatRooms.json", "utf8", (err, data) => {
     if (err) {
       console.log("An error occured while reading JSON Object");
       return;
     }
     const chatRooms = JSON.parse(data);
     chatRooms.rooms[chatUuid] = {
-      "title": props.title,
-      "messages": []
-    }
+      title: props.title,
+      messages: [],
+    };
 
-    fs.writeFile(jsonDirectory + "/chatRooms.json", JSON.stringify(chatRooms), (err) => {
-      if (err) {
-        console.log("An error occured while writing JSON Object");
-        console.log(err);
-        return;
+    fs.writeFile(
+      jsonDirectory + "/chatRooms.json",
+      JSON.stringify(chatRooms),
+      (err) => {
+        if (err) {
+          console.log("An error occured while writing JSON Object");
+          console.log(err);
+          return;
+        }
+        console.log("JSON file has been saved.");
       }
-      console.log("JSON file has been saved.");
-    });
+    );
   });
 
   return chatUuid;
 }
-
 
 /* Runs on both the server and client */
 export default function Index() {
@@ -56,19 +64,28 @@ export default function Index() {
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
       <h1>Welcome to QuickChat!</h1>
       <h2> Click on 'create new chat' to get started</h2>
-      
-      <Form method="post"> 
-        <input name="title" className="shadow appearance-none border rounded py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="chatName" type="text" placeholder="My super cool chat"></input>
-        <button type="submit" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"> Create new Chat </button>
-      </Form>
 
+      <Form method="post">
+        <input
+          name="title"
+          className="shadow appearance-none border rounded py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+          id="chatName"
+          type="text"
+          placeholder="My super cool chat"
+        ></input>
+        <button
+          type="submit"
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+        >
+          {" "}
+          Create new Chat{" "}
+        </button>
+      </Form>
     </div>
   );
 }
 
-export async function action({
-  request,
-}: ActionFunctionArgs) {
+export async function action({ request }: ActionFunctionArgs) {
   console.log("wowza");
   const body = await request.formData();
   const chatId = await createChat({ title: body.get("title") });
--- app/routes/chat.$chatId.tsx
+++ app/routes/chat.$chatId.tsx
@@ -1,28 +1,25 @@
-import { fileURLToPath } from 'url';
-import { dirname } from 'path';
-import { LoaderFunctionArgs } from '@remix-run/node';
-import fs from 'fs';
-import fsPromises from 'fs/promises';
-
-import { useLoaderData } from '@remix-run/react';
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+import { Form } from "@remix-run/react";
+import { LoaderFunctionArgs } from "@remix-run/node";
+import fsPromises from "fs/promises";
+import { useLoaderData } from "@remix-run/react";
+import { useEffect, useRef, useState } from "react";
 
 interface IChat {
   id: string; // the uuid
 }
 
-
-
-export const loader = async({
-    params,
-}: LoaderFunctionArgs) => {
-    console.log("chatId: " + params.chatId);
-    const chatDetails = await getExistingChat(params.chatId);
-    console.log("chatDetails: " + chatDetails);
-    if (!chatDetails) {
-        throw new Response("Could not find chat with UUID", { status: 404 });
-    }
-    console.log("we probably did find the chat Id :)")
-    return chatDetails;
+export const loader = async ({ params }: LoaderFunctionArgs) => {
+  // Connect to the websocket
+  console.log("chatId: " + params.chatId);
+  const chatDetails = await getExistingChat(params.chatId);
+  console.log("chatDetails: " + chatDetails);
+  if (!chatDetails) {
+    throw new Response("Could not find chat with UUID", { status: 404 });
+  }
+  console.log("we probably did find the chat Id :)");
+  return chatDetails;
 };
 
 async function getExistingChat(uuid: string) {
@@ -31,22 +28,77 @@ async function getExistingChat(uuid: string) {
   const jsonDirectory = __dirname + "/../json";
 
   try {
-    const data = await fsPromises.readFile(jsonDirectory + "/chatRooms.json", "utf8");
+    const data = await fsPromises.readFile(
+      jsonDirectory + "/chatRooms.json",
+      "utf8"
+    );
     const chatRooms = JSON.parse(data);
     const room = chatRooms.rooms[uuid];
     return room || null;
   } catch (err) {
     console.error("An error occurred while reading JSON Object", err);
-    return null; // or throw, depending on how you want to handle errors
+    return null;
   }
 }
 
 export default function Chat(props: IChat) {
-    const room = useLoaderData<typeof loader>();
-    return (
-      <div>
-        <h1> Welcome! </h1>
-        <h1>Chat: {room.title}</h1>
-      </div>
-    )
+  const room = useLoaderData<typeof loader>();
+  const ws = useRef<WebSocket | null>(null);
+  const [ message, setMessage ] = useState('');
+  const [receivedMessages, setReceivedMessages] = useState<string[]>([]);
+
+  useEffect(() => {
+    ws.current = new WebSocket(`ws://localhost:8003/${room}`);
+    ws.current.onopen = () => console.log("Connected to websocket");
+    ws.current.onmessage = (event) => {
+      console.log(event.data);
+      const receivedMessage = JSON.parse(event.data);
+      setReceivedMessages(prevMessages => [...prevMessages, receivedMessage]);
+    };
+    ws.current.onopen = () => console.log("ws opened");
+    ws.current.onclose = () => console.log("ws closed");
+
+    return () => {
+      ws.current?.close();
+    };
+  }, [room.id]);
+
+  const sendMessage = async(event: React.FormEvent) => {
+    event.preventDefault();
+    console.log("sending message: " + message);
+    if (ws.current && ws.current.readyState === WebSocket.OPEN) {
+      console.log('wtf');
+      console.log(message);
+      ws.current.send(JSON.stringify({ message }));
+    }
+    setMessage('');
+  };
+
+  return (
+    <div className="flex flex-col align-center">
+      <h1> Welcome! </h1>
+      <h1>Chat: {room.title}</h1>
+      <ul>
+        {receivedMessages.map((msg, index) => (
+          <li key={index}>{msg.message}</li> // Display received messages
+        ))}
+      </ul>
+      <Form onSubmit={sendMessage}>
+        <input
+          name="message"
+          className="shadow appearance-none border rounded py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+          id="chatMessage"
+          type="text"
+          placeholder="My super cool chat"
+          onChange={e => setMessage(e.target.value)}
+          value={message}
+        ></input>
+        <button
+          type="submit"
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+        > Post 
+        </button>
+      </Form>
+    </div>
+  );
 }
